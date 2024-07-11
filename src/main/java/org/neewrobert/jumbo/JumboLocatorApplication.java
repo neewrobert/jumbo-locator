d73dfc86205e134b34227815e5f1a2b661ac6cbc
Newton dos Santos <neewrobert@gmail.com>
2024-07-11 20:23:45 +0200

    Initial project setup with Spring Boot, MongoDB configuration, and dependencies.

---
+++ src/main/java/org/neewrobert/jumbo/JumboLocatorApplication.java
@@ -0,0 +1,13 @@
+package org.neewrobert.jumbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumboLocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumboLocatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/neewrobert/jumbo/JumboLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neewrobert.jumbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumboLocatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
